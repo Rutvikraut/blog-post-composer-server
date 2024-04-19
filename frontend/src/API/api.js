f3058c94bb89279560dfadfddfdcf03e808ac6ec
Rutvik <rutvikraut567@gmail.com>
2024-04-19 22:34:41 +0530

    Cors

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -6,10 +6,13 @@ export const generateData = async (prompt) => {
   const contentapi = "https://blog-post-composer-server.vercel.app/generatecontent";
 
   const data = { prompt };
+  const axiosConfig = {
+    timeout: 30000 // Timeout set to 30 seconds (adjust as needed)
+  };
   const [imgresponse, textresponse, contentresponse] = await Promise.all([
-    axios.post(imgapi, data),
-    axios.post(textapi, data),
-    axios.post(contentapi, data)
+    axios.post(imgapi, data, axiosConfig),
+    axios.post(textapi, data, axiosConfig),
+    axios.post(contentapi, data, axiosConfig)
   ]);
 
   return {
